a53c1c9d1b4237d943d7b2520c4038df0976fc42
ulyana474 <uad474@gmail.com>
2022-07-21 17:35:53 -0700

    delete testconnection.php

--- testconnection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$link = mysqli_connect('127.0.0.1', 'siteadmin', '12345678910');
-if (!$link) {
-die('Could not connect: ' . mysqli_error());
-}
-echo 'Connected successfully';
-mysqli_close($link);
-?>
